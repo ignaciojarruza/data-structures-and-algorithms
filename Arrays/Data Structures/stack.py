5ba1736d52782795aabe99389af3f08c92b4a84b
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-01-26 10:26:50 -0500

    [Data Structure] Stack
    Simple Implementation of a Stack using dynamic arrays (simple is understatement).

---
+++ Arrays/Data Structures/stack.py
@@ -0,0 +1,13 @@
+# A Stack implementation is trivial with dynamic arrays (which I implemented earlier this week)
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, n):
+        self.stack.append(n)
+
+    def pop(self):
+        return self.pop()
+
+    def peek(self):
+        return self.stack[-1]
