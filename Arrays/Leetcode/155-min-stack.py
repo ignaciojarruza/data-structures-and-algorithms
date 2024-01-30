355b0059d9cdfd0d71127e75e455544b1c34f695
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-01-30 10:47:34 -0500

    [Documentation] MinStack class + methods

--- Arrays/Leetcode/155-min-stack.py
+++ Arrays/Leetcode/155-min-stack.py
@@ -12,23 +12,60 @@ Implement the MinStack class:
 You must implement a solution with O(1) time complexity for each function.
 """
 
+"""
+The MinStack class is designed to implement a stack with constant-time retrieval of the 
+minimum element in the stack.
+
+Time Complexity: O(1)
+Space Complexity: O(n)
+"""
+
 
 class MinStack:
+    """
+    Initializes an emptu 'MinStack' object with two empty lists.
+        stack:  for the main stack
+        minStack: for tracking the minimum element at each position
+    """
+
     def __init__(self):
         self.stack = []
         self.minStack = []
+
+    """
+    Adds the specified value to the main stack. Determines the minimum value
+    up to the current position and updates the 'minStack' accordignly.
+    
+    Parameters:
+        val(int)    : The value to be pushed onto the stack.
+    """
 
     def push(self, val: int) -> None:
         self.stack.append(val)
         val = min(val, self.minStack[-1] if self.minStack else val)
         self.minStack.append(val)
 
+    """
+    Removes the top element from both the main stack and the minStack.
+    """
+
     def pop(self) -> None:
         self.stack.pop()
         self.minStack.pop()
 
+    """
+    Returs the top element of the main stack (without removal).
+    
+    Returns:
+        int: Top element of the miain stack.
+    """
+
     def top(self) -> int:
         return self.stack[-1]
+
+    """
+    Returns the minimum element in the main stack without removing it.
+    """
 
     def getMin(self) -> int:
         return self.minStack[-1]
