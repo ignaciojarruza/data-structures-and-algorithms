a84518a9f7778388c654167ff834ec82e9b5247a
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-03-11 16:52:43 -0400

    [Documentation] Queue
    Full documentation of implementation.
    - Queue class
    - init()
    - enqueue(val)
    - dequeue()

--- dataStructures/queue.py
+++ dataStructures/queue.py
@@ -1,10 +1,31 @@
 from Helper.ListNode import ListNode
         
 class Queue:
+    """
+    Implementation of a Queue data structure.
+
+    Attributes:
+        left (ListNode): Pointer to the leftmost node in the queue.
+        right (ListNode): Pointer to the rightmost node in the queue.
+
+    Methods:
+        __init__(): Initializes an empty Queue.
+        enqueue(val): Adds a new element to the right end of the queue.
+        dequeue(): Removes and returns the leftmost element from the queue.
+    """
     def __init__(self):
+        """
+        Initializes an empty Queue.
+        """
         self.left = self.right = None
 
     def enqueue(self, val):
+        """
+        Adds a new element to the right end of the queue.
+
+        Args:
+            val: The value to be added to the queue.
+        """
         #Create new node to add
         newNode = ListNode(val)
 
@@ -18,6 +39,12 @@ class Queue:
             self.left = self.right = newNode   
 
     def dequeue(self):
+        """
+        Removes and returns the leftmost element from the queue.
+        
+        Returns:
+            The value of the leftmost element in the queue. Return None if the queue is empty.
+        """
         # Queue is empty
         if not self.left:
             return None
